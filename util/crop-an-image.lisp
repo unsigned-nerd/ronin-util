ed9bf15e2479a28038ecd585bbb0c0c27da1bf09
unsigned_nerd <unsigned_nerd@member.fsf.org>
2020-05-24 10:56:31 +0700

    To crop an image, no need to write any extra util, just simply use Ronin's function and helper

---
+++ util/crop-an-image.lisp
@@ -0,0 +1,10 @@
+; It is easy to do by Ronin's function and helper.  At the editor, you type:
+;
+;   (crop $rect
+;
+; Then you drag an area on the image you want to crop.  The $rect is replaced with the dimensions.
+; At the editor, you type:
+;
+;   )
+;
+; Then, you press Ctrl+Enter.
